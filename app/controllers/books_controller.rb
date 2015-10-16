a240246f17df20f2e0e740848fd2bdbf44bd3f73
Dmitriy K <fobfreeman@gmail.com>
2015-10-16 13:28:46 +0000

    added book to user association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+                
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
@@ -25,7 +27,11 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+  
+    @book = current_user.books.new(book_params)
+    @book.save
+    # respond_with(@book)
+      
 
     respond_to do |format|
       if @book.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015224015) do
+ActiveRecord::Schema.define(version: 20151016130859) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151015224015) do
     t.boolean  "avaliability", default: true
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
