70062c7936163712e181a8dfbaed5e51dd087eb7
Dmitriy K <fobfreeman@gmail.com>
2015-10-16 12:01:03 +0000

    added book partial, updated index view and avaliablility

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+         @books = Book.where(avaliability: true)
+         
   end
 
   # GET /books/1
