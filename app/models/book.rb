38c440b90e41472d115baf5394a5a4ac2474ec8d
Dmitriy K <fobfreeman@gmail.com>
2015-10-26 11:54:29 +0000

    added sales model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image
   has_attached_file :resource
+  has_many :sales
   
   
   validates_attachment_content_type :image, 
@@ -17,4 +18,5 @@ message: "Only pdfs allowed"
 
 validates :image, attachment_presence: true
 validates :resource, attachment_presence: true
+validated_numericality_of :price, greater_than: 49, message: "Must be at least 50 cents."
 end
