f2745859d0ce9853b802e58a93bcbb588ca495ac
Dmitriy K <fobfreeman@gmail.com>
2015-10-30 02:08:02 +0000

    Added states to sales

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026112958) do
+ActiveRecord::Schema.define(version: 20151030011411) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 20151026112958) do
     t.integer  "book_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "state"
+    t.string   "stripe_id"
+    t.string   "stripe_token"
+    t.text     "error"
   end
 
   create_table "users", force: :cascade do |t|
